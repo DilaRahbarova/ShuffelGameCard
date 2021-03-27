d64a35b89440ef1f8010daae36fc82353202f7ef
DilaRahbarova <rahbarovadila@gmail.com>
2021-03-27 10:10:31 -0400

    first commit

---
+++ src/test/java/gameCard/PlayTheGame.java
@@ -0,0 +1,12 @@
+package gameCard;
+
+import java.util.Scanner;
+
+public class PlayTheGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the game!\nEnter the amount of players to begin: ");
+        int playerNum = input.nextInt();
+    }
+
+}
